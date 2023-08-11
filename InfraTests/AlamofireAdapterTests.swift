a892f734869478a7f2e23ff63102cbdd0b44ef1b
Gabriel Chacon <gabrielchaconbh@gmail.com>
2023-08-11 19:20:11 -0300

    Add infra layer

---
+++ InfraTests/AlamofireAdapterTests.swift
@@ -0,0 +1,15 @@
+//
+//  AlamofireAdapterTests.swift
+//  AlamofireAdapterTests
+//
+//  Created by Gabriel Chacon on 11/08/23.
+//
+
+import XCTest
+
+final class AlamofireAdapterTests: XCTestCase {
+
+    func test_() {
+    }
+
+}
