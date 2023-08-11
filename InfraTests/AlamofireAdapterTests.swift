dae887dd4f202dce9dc39d14d109bca18402990b
Gabriel Chacon <gabrielchaconbh@gmail.com>
2023-08-11 20:35:45 -0300

    Alamofire install and the first test

--- InfraTests/AlamofireAdapterTests.swift
+++ InfraTests/AlamofireAdapterTests.swift
@@ -6,10 +6,58 @@
 //
 
 import XCTest
+import Alamofire
 
-final class AlamofireAdapterTests: XCTestCase {
+class AlamofireAdapter {
+    
+    private var session: Session
+    
+    init(session: Session = .default) {
+        self.session = session
+    }
+    
+    func get(to url: URL) {
+        session.request(url).resume()
+    }
+}
 
+class AlamofireAdapterTests: XCTestCase {
+    
     func test_() {
+        let url = makeUrl()
+        let configuration = URLSessionConfiguration.default
+        configuration.protocolClasses = [UrlProtocolStub.self]
+        let session = Session(configuration: configuration)
+        let sut = AlamofireAdapter(session: session)
+        sut.get(to: url)
+        let exp = expectation(description: "waiting")
+        UrlProtocolStub.observeRequest { request in
+            XCTAssertEqual(url, request.url)
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1)
+    }
+}
+
+class UrlProtocolStub: URLProtocol {
+    
+    static var emit: ((URLRequest) -> Void)?
+    
+    static func observeRequest(completion: @escaping (URLRequest) -> Void) {
+        UrlProtocolStub.emit = completion
     }
 
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+    
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+    
+    override func startLoading() {
+        UrlProtocolStub.emit?(request)
+    }
+    
+    override func stopLoading() {}
 }
