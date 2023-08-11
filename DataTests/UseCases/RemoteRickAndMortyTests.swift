510a976605ac58d3edcb9bfe4163a01ee66e762a
Gabriel Chacon <gabrielchaconbh@gmail.com>
2023-08-11 18:27:26 -0300

    Simplify code helpers

--- DataTests/UseCases/RemoteRickAndMortyTests.swift
+++ DataTests/UseCases/RemoteRickAndMortyTests.swift
@@ -5,7 +5,7 @@ import Data
 final class RemoteRickAndMortyTests: XCTestCase {
     
     func test_get_should_call_httpClient_with_correct_url() {
-        let url = URL(string: "http://something.com")!
+        let url = makeUrl()
         let (sut, httpClientSpy) = makeSut(url: url)
         sut.getRickAndMorty() { _ in }
         XCTAssertEqual(httpClientSpy.urls, [url])
@@ -29,7 +29,7 @@ final class RemoteRickAndMortyTests: XCTestCase {
     func test_get_should_complete_with_error_if_client_completes_with_invalid_date() {
         let (sut, httpClientSpy) = makeSut()
         expect(sut, completeWith: .failure(.unexpected), when: {
-            httpClientSpy.completeWithData(Data("invalid_data".utf8))
+            httpClientSpy.completeWithData(makeInvalidData())
         })
     }
 }
@@ -64,6 +64,14 @@ extension RemoteRickAndMortyTests {
     
     func makeRickAndMortyModel() -> RickAndMortyModel {
         RickAndMortyModel(characters: "http://characters-url.com")
+    }
+    
+    func makeUrl() -> URL {
+        URL(string: "http://something.com")!
+    }
+    
+    func makeInvalidData() -> Data {
+        Data("invalid_data".utf8)
     }
     
     class HttpClientSpy: HttpGetClientProtocol {
