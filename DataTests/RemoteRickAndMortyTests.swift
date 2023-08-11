eafe033f721ece38f75abe1590e63f079ba77fda
Gabriel Chacon <gabrielchaconbh@gmail.com>
2023-08-11 15:55:30 -0300

    Refactory but method

--- DataTests/RemoteRickAndMortyTests.swift
+++ DataTests/RemoteRickAndMortyTests.swift
@@ -11,31 +11,37 @@ class RemoteRickAndMorty {
     }
     
     func getRickAndMorty() {
-        httpClient.get(url: self.url)
+        httpClient.get(to: url)
     }
 }
 
 protocol HttpGetClient {
-    func get(url: URL)
+    func get(to url: URL)
 }
 
 final class RemoteRickAndMortyTests: XCTestCase {
     
     func test_get_should_call_httpClient_with_correct_url() {
         let url = URL(string: "http://something.com")!
-        let httpClientSpy = HttpClientSpy()
-        let sut = RemoteRickAndMorty(url: url, httpClient: httpClientSpy)
+        let (sut, httpClientSpy) = makeSut(url: url)
         sut.getRickAndMorty()
         XCTAssertEqual(httpClientSpy.url, url)
-    }    
+    }
 }
 
 extension RemoteRickAndMortyTests {
+    
+    func makeSut(url: URL = URL(string: "http://default.com")!) -> (sut: RemoteRickAndMorty, httpClientSpy: HttpClientSpy) {
+        let httpClientSpy = HttpClientSpy()
+        let sut = RemoteRickAndMorty(url: url, httpClient: httpClientSpy)
+        return (sut, httpClientSpy)
+    }
+    
     class HttpClientSpy: HttpGetClient {
         
         var url: URL?
         
-        func get(url: URL) {
+        func get(to url: URL) {
             self.url = url
         }
     }
