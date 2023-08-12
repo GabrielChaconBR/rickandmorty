9fd6f48222e567168fb22eab563c044c154f084e
Gabriel Chacon <gabrielchaconbh@gmail.com>
2023-08-12 10:50:52 -0300

    Add ViewModel layer

---
+++ ViewModelTests/RickAndMortyViewModelTests.swift
@@ -0,0 +1,16 @@
+//
+//  RickAndMortyViewModelTests.swift
+//  RickAndMortyViewModelTests
+//
+//  Created by Gabriel Chacon on 12/08/23.
+//
+
+import XCTest
+
+final class RickAndMortyViewModelTests: XCTestCase {
+
+    func test_() {
+        
+    }
+
+}
